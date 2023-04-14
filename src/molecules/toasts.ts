16b7b9aa23e95916f623a4fccf718581397a98be
Bryce Curtis <curtis.bryce@gmail.com>
2023-04-14 12:14:36 -0400

    Change gap prop name to padding and is a multipier

--- src/molecules/toasts.ts
+++ src/molecules/toasts.ts
@@ -14,15 +14,15 @@ export class Toasts extends Molecule {
     
     /** The handle border radius property */
     public handleBorderRadius: PropertyNumberSelectable;
-    /** The gap property */
-    public gap: PropertyPixelSelectable;
+    /** The padding property */
+    public padding: PropertyNumberSelectable;
     /** The elevation property */
     public elevation: PropertyElevationSelectable;
 
     constructor(molecules: IMolecules) {
         super("Toasts", molecules);
         this.handleBorderRadius = new PropertyNumberSelectable("Handle Border Radius", false, this, [0,1,2,3,4,5,6,7,8], 1);
-        this.gap = new PropertyPixelSelectable("Gap", false, this, [8,16,24,32,40,44], 24);
+        this.padding = new PropertyNumberSelectable("Padding", false, this, [1,2,3], 1);
         this.elevation = new PropertyElevationSelectable("Toast Elevation", false, this, 0, 9);
     }
 
@@ -30,14 +30,14 @@ export class Toasts extends Molecule {
         if (!obj) return;
         super.deserialize(obj);
         this.handleBorderRadius.deserialize(obj.handleBorderRadius);
-        this.gap.deserialize(obj.gap);
+        this.padding.deserialize(obj.gap);
         this.elevation.deserialize(obj.elevation);
     }
 
     public serialize(): any {
         const obj: any = {};
         obj.handleBorderRadius = this.handleBorderRadius.serialize();
-        obj.gap = this.gap.serialize();
+        obj.gap = this.padding.serialize();
         obj.elevation = this.elevation.serialize();
         return obj;
     }
